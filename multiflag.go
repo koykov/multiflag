139a06b6203f1c959e44f50768402d5ace51de16
Konstantin Voykov <koykov0@gmail.com>
2023-07-27 22:22:01 +0300

    Implement option aliases.

--- multiflag.go
+++ multiflag.go
@@ -8,29 +8,46 @@ import (
 )
 
 type MultiFlag struct {
-	s *flag.FlagSet
+	s     *flag.FlagSet
+	alias map[string][]string
 }
 
 func NewMultiFlag(name string, errorHandling flag.ErrorHandling) *MultiFlag {
-	f := MultiFlag{s: flag.NewFlagSet(name, errorHandling)}
+	f := MultiFlag{
+		s:     flag.NewFlagSet(name, errorHandling),
+		alias: make(map[string][]string),
+	}
 	f.s.Usage = f.defaultUsage
 	return &f
 }
 
 func (f *MultiFlag) BoolVars(p *bool, names []string, value bool, usage string) {
-	names = uniqStr(names)
-	for i := 0; i < len(names); i++ {
-		f.fs().BoolVar(p, names[i], value, usage)
-	}
+	f.var_(p, names, value, usage)
 }
 
-func (f *MultiFlag) StringVars(p *string, names []string, value string, usage string) {}
+func (f *MultiFlag) StringVars(p *string, names []string, value string, usage string) {
+	f.var_(p, names, value, usage)
+}
 
 func (f *MultiFlag) PrintDefaults() {
 	var isZeroValueErrs []error
+	var skip = map[string]struct{}{}
 	f.VisitAll(func(flag2 *flag.Flag) {
+		if _, ok := skip[flag2.Name]; ok {
+			return
+		}
 		var b strings.Builder
-		_, _ = fmt.Fprintf(&b, "  -%s", flag2.Name) // Two spaces before -; see next two comments.
+		_, _ = fmt.Fprintf(&b, "  -%s", flag2.Name)
+		if aliases, ok := f.alias[flag2.Name]; ok {
+			for i := 0; i < len(aliases); i++ {
+				a := aliases[i]
+				if _, ok = skip[a]; ok || a == flag2.Name {
+					continue
+				}
+				_, _ = fmt.Fprintf(&b, ", -%s", a)
+				skip[a] = struct{}{}
+			}
+		}
 		name, usage := flag.UnquoteUsage(flag2)
 		if len(name) > 0 {
 			b.WriteString(" ")
@@ -62,6 +79,21 @@ func (f *MultiFlag) PrintDefaults() {
 	}
 }
 
+func (f *MultiFlag) var_(p any, names []string, value any, usage string) {
+	if names = uniqStr(names); len(names) > 0 {
+		key := names[0]
+		for i := 0; i < len(names); i++ {
+			switch value.(type) {
+			case bool:
+				f.fs().BoolVar(p.(*bool), names[i], value.(bool), usage)
+			case string:
+				f.fs().StringVar(p.(*string), names[i], value.(string), usage)
+			}
+			f.alias[key] = append(f.alias[key], names[i])
+		}
+	}
+}
+
 func isZeroValue(flag *flag.Flag, value string) (ok bool, err error) {
 	return flag.Value.String() == value, nil
 }
@@ -69,6 +101,9 @@ func isZeroValue(flag *flag.Flag, value string) (ok bool, err error) {
 func (f *MultiFlag) fs() *flag.FlagSet {
 	if f.s == nil {
 		f.s = flag.NewFlagSet("", flag.ExitOnError)
+	}
+	if f.alias == nil {
+		f.alias = make(map[string][]string)
 	}
 	return f.s
 }
